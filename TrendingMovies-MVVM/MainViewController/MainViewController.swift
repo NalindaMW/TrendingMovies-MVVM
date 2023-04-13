ec7b518bcbc7eeb56d7cbf7e773aac90fdea1f8d
Nalinda Wickramarathna <mastersilicon@gmail.com>
2023-04-13 16:56:06 +0530

    Removed Main storyboard, Scene delegate, view controller and introduce Main view controller in app delegate

---
+++ TrendingMovies-MVVM/MainViewController/MainViewController.swift
@@ -0,0 +1,28 @@
+//
+//  MainViewController.swift
+//  TrendingMovies-MVVM
+//
+//  Created by Nalinda Wickramarathna on 2023-04-13.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.title = "Main View"
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
