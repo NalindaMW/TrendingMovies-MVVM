4ccf72969b6fa566e07134784ac2167d2e2a78c8
Nalinda Wickramarathna <mastersilicon@gmail.com>
2023-04-13 15:33:38 +0530

    Initial commit

---
+++ TrendingMovies-MVVM/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TrendingMovies-MVVM
+//
+//  Created by Nalinda Wickramarathna on 2023-04-13.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
