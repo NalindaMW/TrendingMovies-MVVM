adf3ded3ac21749a3a37ad215ec2019054c0afcb
Nalinda Wickramarathna <mastersilicon@gmail.com>
2023-04-14 11:58:00 +0530

    Added basic data from MainViewModel

---
+++ TrendingMovies-MVVM/Controllers/MainViewController/MainViewController.swift
@@ -0,0 +1,30 @@
+//
+//  MainViewController.swift
+//  TrendingMovies-MVVM
+//
+//  Created by Nalinda Wickramarathna on 2023-04-13.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    //@IBOutlets:
+    @IBOutlet weak var tableView: UITableView!
+    
+    //ViewModel:
+    var viewModel: MainViewModel = MainViewModel()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.configView()
+    }
+
+    func configView() {
+        self.title = "Main View 1"
+        
+        self.setupTableView()
+    }
+
+}
---
+++ TrendingMovies-MVVM/ViewModel/MainViewModel.swift
@@ -0,0 +1,19 @@
+//
+//  MainViewModel.swift
+//  TrendingMovies-MVVM
+//
+//  Created by Nalinda Wickramarathna on 2023-04-13.
+//
+
+import Foundation
+
+class MainViewModel {
+    
+    func numberOfSections() -> Int {
+        return 1
+    }
+    
+    func numberOfRows(in section: Int) -> Int {
+        return 10
+    }
+}
